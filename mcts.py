8043803aca544e824a21cf45b443c4f6d9e9145d
Lyn BIA <929468893@qq.com>
2020-12-23 11:35:03 +0800

    altered mcts.py and single score in board_scorer.py

--- mcts.py
+++ mcts.py
@@ -1,4 +1,4 @@
-from board_scorer import Scorer,PatternExtractionScorer
+from board_scorer import *
 import random
 from itertools import product
 import copy 
@@ -22,7 +22,7 @@ class MCTSScorer(Scorer):
     Scorer that is based on the MCTS method
     """
     @staticmethod
-    def evaluate(board, x, y, move,trail=500):
+    def evaluate(board, x, y, move,trail=200,explore=4):
         """
         This function takes a board, evaluates the score (on behave of player 1)
         and returns the evaluated score
@@ -32,17 +32,33 @@ class MCTSScorer(Scorer):
         :param y:   last recent move's position y
         :param move: last recent move's player, move = 1 / 2
         :param trail:   number of simulations at each action
+        :param explore: number of opponent's actions to be explored
         :return: a real value for the score of this recent move (the best winning rate among the following actions)
         """
         new_board = copy.deepcopy(board)
         new_board[x][y] = move # the player takes the current move 
-        t = 0
-        win = 0
-        while (t < trail):
-            t += 1
-            win += MCTSScorer.simulation(new_board,move,player = 3 - move) # simulate winner 
-        return (win/t)
-        # raise NotImplementedError("Implement it")
+        actions = MCTSScorer.findNeighbor(new_board,neighborNearBy=1)
+        score = {}
+        for action in actions:
+            a_board = copy.deepcopy(new_board)
+            a_board[action[0]][action[1]] = 3 - move
+            score[action] = MCTSScorer.heuristic(a_board,action[0],action[1],3 - move)
+
+        score = sorted(score.items(), key = lambda s:(s[1], s[0]))
+        winrate = [0 for i in range(explore)]
+        for i in range(explore):
+            t = 0
+            win = 0
+            action = score[i][0]
+            a_board = copy.deepcopy(new_board)
+            a_board[action[0]][action[1]] = 3 - move
+            while (t < trail):
+                t += 1
+                win += MCTSScorer.simulation(copy.deepcopy(a_board),move,player = move) 
+            winrate[i] = win/t
+            print(win)
+        print(winrate)
+        return min(winrate) # 返回最小胜率（minimax思想）
 
     @staticmethod
     def simulation(board,move,player):
@@ -50,35 +66,36 @@ class MCTSScorer(Scorer):
         To simulate a game randomly according to current board,
         :return: the winner
         """
-        winner = MCTSScorer.judge(board,move)
-        if winner == move:
-            return 1
-        elif winner == 3 - move:
-            return 0
+        winner = MCTSScorer.judge(board)
+        if winner != 0:
+            # print(winner == move)
+            return (winner == move)
         else:
-            board[MCTSRandomMove.next_move(board,player)] = player
+            action = MCTSRandomMove.next_move(board,player)
+            board[action[0]][action[1]] = player
             return MCTSScorer.simulation(board,move,3 - player)
 
     @staticmethod
-    def judge(board, move):
+    def judge(board):
         """
-        To check if a final state is reached
+        To check if a final state is reached and the winner
         """
         patterDict = PatternExtractionScorer.patternCount(board)
-        if patterDict['11111'][move - 1] or patterDict['011110'][move - 1]:
-            return move
-        elif patterDict['11111'][2 - move] or patterDict['011110'][2 - move]:
-            return 3 - move
+        if patterDict['11111'][0] or patterDict['011110'][0]:
+            # for i in board:print(i)
+            return 1
+        elif patterDict['11111'][1] or patterDict['011110'][1]:
+            # for i in board:print(i)
+            return 2
         return 0 
 
     @staticmethod
-    def heuristic(board, move):
+    def heuristic(board,x,y,move):
         """
         This function is used during the internal nodes pruning
         This method is used for better pruning strategy
-        TODO is it really needed for MCTS?
         """
-        return 0
+        return PatternExtractionScorer.evaluate(board,x,y,move)
     
     @staticmethod
     def findNeighbor(board, neighborNearBy=1):
@@ -98,3 +115,26 @@ class MCTSScorer(Scorer):
                     neighbors.append((i, j))
         return neighbors
 
+
+if __name__ == "__main__":
+    board = [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    print(MCTSScorer.evaluate(board,0,1,1))
