e7f0e8d9edc099bd8e1beb26c5dbb68cc60ead00
Lyn BIA <929468893@qq.com>
2020-12-23 09:53:59 +0800

    add mcts.py

--- mcts.py
+++ mcts.py
@@ -1,6 +1,7 @@
-from board_scorer import Scorer
+from board_scorer import Scorer,PatternExtractionScorer
 import random
-
+from itertools import product
+import copy 
 
 class MCTSMovePicker:
     @staticmethod
@@ -11,7 +12,9 @@ class MCTSMovePicker:
 class MCTSRandomMove(MCTSMovePicker):
     @staticmethod
     def next_move(board, player):
-        raise NotImplementedError("Implement me")
+        actions = MCTSScorer.findNeighbor(board,neighborNearBy=1)
+        return random.choice(actions)
+        # raise NotImplementedError("Implement me")
 
 
 class MCTSScorer(Scorer):
@@ -19,7 +22,7 @@ class MCTSScorer(Scorer):
     Scorer that is based on the MCTS method
     """
     @staticmethod
-    def evaluate(board, x, y, move):
+    def evaluate(board, x, y, move,trail=500):
         """
         This function takes a board, evaluates the score (on behave of player 1)
         and returns the evaluated score
@@ -28,15 +31,70 @@ class MCTSScorer(Scorer):
         :param x:   last recent move's position x
         :param y:   last recent move's position y
         :param move: last recent move's player, move = 1 / 2
-        :return: a real value for the score of this recent move
+        :param trail:   number of simulations at each action
+        :return: a real value for the score of this recent move (the best winning rate among the following actions)
         """
-        raise NotImplementedError("Implement it")
+        new_board = copy.deepcopy(board)
+        new_board[x][y] = move # the player takes the current move 
+        t = 0
+        win = 0
+        while (t < trail):
+            t += 1
+            win += MCTSScorer.simulation(new_board,move,player = 3 - move) # simulate winner 
+        return (win/t)
+        # raise NotImplementedError("Implement it")
 
     @staticmethod
-    def heuristic(board, x, y, move):
+    def simulation(board,move,player):
+        """
+        To simulate a game randomly according to current board,
+        :return: the winner
+        """
+        winner = MCTSScorer.judge(board,move)
+        if winner == move:
+            return 1
+        elif winner == 3 - move:
+            return 0
+        else:
+            board[MCTSRandomMove.next_move(board,player)] = player
+            return MCTSScorer.simulation(board,move,3 - player)
+
+    @staticmethod
+    def judge(board, move):
+        """
+        To check if a final state is reached
+        """
+        patterDict = PatternExtractionScorer.patternCount(board)
+        if patterDict['11111'][move - 1] or patterDict['011110'][move - 1]:
+            return move
+        elif patterDict['11111'][2 - move] or patterDict['011110'][2 - move]:
+            return 3 - move
+        return 0 
+
+    @staticmethod
+    def heuristic(board, move):
         """
         This function is used during the internal nodes pruning
         This method is used for better pruning strategy
         TODO is it really needed for MCTS?
         """
         return 0
+    
+    @staticmethod
+    def findNeighbor(board, neighborNearBy=1):
+        """
+        Find all open positions 1 cell away
+        TODO Enable more general neighbor discovering strategy
+        :param board:
+        :param neighborNearBy: how much distance
+        :return: a list of binary tuples.
+        """
+        neighbors = []
+        stones = [(i, j) for i in range(len(board)) for j in range(len(board)) if board[i][j] != 0]
+        for stone in stones:
+            for i, j in product(range(stone[0] - 1, stone[0] + 2), range(stone[1] - 1, stone[1] + 2)):
+                if (i, j) in product(range(len(board)), range(len(board))) and (i, j) not in neighbors and (
+                        i, j) not in stones:
+                    neighbors.append((i, j))
+        return neighbors
+
